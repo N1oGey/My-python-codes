ce88df338a5e6efa04e8a2fb103f6d82f0d08055
evilrobloxian <ivasuraivan11@gmail.com>
2026-05-25 08:47:12 +0300

    Create activation_code_1.py

---
+++ activation_code_1.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import webbrowser
+
+app = Flask(__name__)
+
+ACTIVATION_CODE = "1234"
+
+@app.route("/open")
+def open_link():
+    code = request.args.get("code")
+    url = request.args.get("url")
+
+    if code == ACTIVATION_CODE and url:
+        webbrowser.open(url)
+        return "OK"
+    return "DENIED"
+
+app.run(host="0.0.0.0", port=5000)
