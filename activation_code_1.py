afaffbd9d3d2f0cfba5041ec9a08a58d9ae16692
evilrobloxian <ivasuraivan11@gmail.com>
2026-05-25 19:18:43 +0300

    Update activation_code_1.py

--- activation_code_1.py
+++ activation_code_1.py
@@ -7,13 +7,12 @@ ACTIVATION_CODE = "1234"
 
 @app.route("/open")
 def open_link():
-    code = request.args.get("code", "")
-    url = request.args.get("url", "")
+    code = request.args.get("code")
+    url = request.args.get("url")
 
     if code == ACTIVATION_CODE and url:
         webbrowser.open(url)
         return "OK"
     return "DENIED"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", port=5000)
